167486cfcd480f83c5cf6407ff84db148223d8f4
cihan sungur <sungur88@windowslive.com>
2021-12-27 21:08:00 +0300

    AppDbContextAdded

---
+++ UdemyNLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyNLayerProject.Core.Entities;
+
+namespace UdemyNLayerProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
