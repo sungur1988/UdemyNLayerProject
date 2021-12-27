7e8a8b89d777feef109704f2c890e5bbd14b5322
cihan sungur <sungur88@windowslive.com>
2021-12-27 21:12:39 +0300

    ProductConfigurationAdded

--- UdemyNLayerProject.Data/AppDbContext.cs
+++ UdemyNLayerProject.Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UdemyNLayerProject.Core.Entities;
+using UdemyNLayerProject.Data.Configurations;
 
 namespace UdemyNLayerProject.Data
 {
@@ -18,7 +19,7 @@ namespace UdemyNLayerProject.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
         }
     }
 }
