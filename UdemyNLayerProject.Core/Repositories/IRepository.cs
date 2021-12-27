71f38258927d5c4935c8c4acdd1489b10b10a490
cihan sungur <sungur88@windowslive.com>
2021-12-27 20:20:23 +0300

    RepositoryIntefacesAdded

---
+++ UdemyNLayerProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+
+        Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate=null);
+
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity Update(TEntity entity);
+    }
+}
