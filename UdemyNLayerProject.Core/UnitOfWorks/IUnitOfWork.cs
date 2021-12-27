c8ddd033b242b2ee6bbda50181eb313f18bfab21
cihan sungur <sungur88@windowslive.com>
2021-12-27 20:58:24 +0300

    IUnitOfWorkInterfaceAdded

---
+++ UdemyNLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository productRepository { get; }
+        ICategoryRepository categoryRepository { get; }
+
+        void Commit();
+        Task CommitAsync();
+
+    }
+}
