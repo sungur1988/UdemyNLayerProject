ce33f00000e16c79d4c932c6a9414fe2e6b784ea
cihan sungur <sungur88@windowslive.com>
2021-12-27 21:41:46 +0300

    ProductRepositoryAdded

---
+++ UdemyNLayerProject.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Entities;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await appDbContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x=>x.Id==productId);
+        }
+    }
+}
