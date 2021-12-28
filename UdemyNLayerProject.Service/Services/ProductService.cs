410dcf1d3842acfee07d860b327d322e3aba1e9f
cihan sungur <sungur88@windowslive.com>
2021-12-29 00:27:36 +0300

    ProductServiceAdded

---
+++ UdemyNLayerProject.Service/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Entities;
+using UdemyNLayerProject.Core.Repositories;
+using UdemyNLayerProject.Core.Services;
+using UdemyNLayerProject.Core.UnitOfWorks;
+
+namespace UdemyNLayerProject.Service.Services
+{
+    public class ProductService : GenericService<Product>, IProductService
+    {
+
+        public ProductService(IUnitOfWork unitOfWork,IRepository<Product> repository) : base(unitOfWork,repository)
+        {
+            
+        }
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await _unitOfWork.productRepository.GetWithCategoryByIdAsync(productId);
+        }
+    }
+}
