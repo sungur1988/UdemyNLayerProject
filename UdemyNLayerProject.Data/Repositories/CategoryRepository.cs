03d87b9317869dfbfe48e27d5f6460c2a69b7503
cihan sungur <sungur88@windowslive.com>
2021-12-28 23:37:17 +0300

    Dependecies&MigrationAdded

---
+++ UdemyNLayerProject.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Entities;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Data.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+           return await appDbContext.Categories.Include(x => x.Products).SingleOrDefaultAsync(x => x.Id == categoryId);
+        }
+    }
+}
---
+++ UdemyNLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+using UdemyNLayerProject.Core.UnitOfWorks;
+using UdemyNLayerProject.Data.Repositories;
+
+namespace UdemyNLayerProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private AppDbContext _appDbContext;
+        private CategoryRepository _categoryRepository;
+        private ProductRepository _productRepository;
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IProductRepository productRepository { get {
+                if (_productRepository==null)
+                {
+                    _productRepository = new ProductRepository(_appDbContext);
+                }
+
+                return _productRepository;
+            
+            } }/*=> _productRepository = _productRepository ?? new ProductRepository(_appDbContext);*/
+
+        public ICategoryRepository categoryRepository => _categoryRepository = _categoryRepository ?? new CategoryRepository(_appDbContext);
+
+        public void Commit()
+        {
+            _appDbContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
